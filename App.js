1ad30971c76ce34b4678df52ee11ce0a704e6473
anandprakash01 <hr.anand0101@gmail.com>
2023-04-04 05:14:00 +0530

    Uploading files

---
+++ App.js
@@ -0,0 +1,250 @@
+
+
+const API_KEY = "api_key=d6a90eff85ca8ee94564a85d832ee40a";
+const BASE_URL = "https://api.themoviedb.org/3";
+// const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const API_URL = BASE_URL + "/movie/now_playing?" + API_KEY;
+
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const search_URL = BASE_URL + "/search/movie?" + API_KEY;
+
+const movieContainer = document.getElementById("movies-container");
+const search = document.getElementById("search");
+const searchBtn = document.getElementById("nav-search-btn");
+const tagsEl = document.getElementById("tags");//for genres
+
+const prev = document.getElementById("pre");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+
+//varibles for pagination
+let currentPage = 1;
+let nextPage = 2;
+let prevPage = 3;
+let lastURL = "";
+let totalPages = 10;
+
+const genres = [
+    { "id": 28, "name": "Action" },
+    { "id": 12, "name": "Adventure" },
+    { "id": 16, "name": "Animation" },
+    { "id": 35, "name": "Comedy" },
+    { "id": 80, "name": "Crime" },
+    { "id": 99, "name": "Documentary" },
+    { "id": 18, "name": "Drama" },
+    { "id": 10751, "name": "Family" },
+    { "id": 14, "name": "Fantasy" },
+    { "id": 36, "name": "History" },
+    { "id": 27, "name": "Horror" },
+    { "id": 10402, "name": "Music" },
+    { "id": 9648, "name": "Mystery" },
+    { "id": 10749, "name": "Romance" },
+    { "id": 878, "name": "Science Fiction" },
+    { "id": 10770, "name": "TV Movie" },
+    { "id": 53, "name": "Thriller" },
+    { "id": 10752, "name": "War" },
+    { "id": 37, "name": "Western" }
+];
+
+let selectedGenres = [];
+
+getMovies(API_URL);
+
+setGenres();
+
+
+function getMovies(url) {
+    // fetch(url).then((res)=>{
+    //     return res.json();
+    // }).then((data)=>{
+    //     // console.log(data);
+    //     showMovies(data.results);
+    // })
+    lastURL = url;
+    fetch(url).then(res => res.json()).then(data => {
+        // console.log(data);
+        if (data.results.length != 0) {
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = data.page + 1;
+            prevPage = data.page - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+            if (currentPage <= 1) {
+                prev.classList.add('disabled');
+                next.classList.remove('disabled')
+            }
+            else if (currentPage >= totalPages) {
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+            }
+            else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled')
+            }
+            // tagsEl.scrollIntoView({behavior:"smooth"});
+            // search.scrollIntoView({behavior:"smooth"});
+
+        }
+        else {
+            movieContainer.innerHTML = `<h1>No Results Found</h1>`;
+        }
+    })
+}
+
+function showMovies(data) {
+    movieContainer.innerHTML = ' ';
+
+    data.forEach(movies => {
+
+        const { original_language, poster_path, title, vote_average, overview } = movies;
+        const movieElement = document.createElement('div');
+        movieElement.classList.add("movie-card");
+        movieElement.innerHTML = `
+            <img src="${poster_path ? IMG_URL + poster_path : "./photo.jpg"}">
+            <div>${title}</div>
+                <div class="lang-rating">
+                <p>${original_language}</p>
+                <p>${vote_average}</p>
+            </div>
+        `
+
+        movieContainer.appendChild(movieElement);
+    });
+}
+
+
+// Search functions
+search.addEventListener("input", () => {
+    const searchTxt = search.value;
+    //to remove filters
+    selectedGenres = [];
+    setGenres();
+
+    if (searchTxt) {
+        getMovies(search_URL + "&query=" + searchTxt);
+    }
+    else {
+        getMovies(API_URL);
+    }
+
+})
+// function handle(e){
+//     if (e.keyCode==13){
+//         getMovies(search_URL+"&query="+searchTxt);
+//     }
+//     else{
+//         getMovies(API_URL);
+//     }
+// }
+searchBtn.addEventListener("click", () => {
+    const searchTxt = search.value;
+    if (searchTxt) {
+        getMovies(search_URL + "&query=" + searchTxt);
+    }
+    else {
+        getMovies(API_URL);
+    }
+});
+
+
+//genres fitltering
+function setGenres() {
+    tagsEl.innerHTML = "";
+    genres.forEach(genre => {
+        const t = document.createElement("div");
+        t.classList.add("genres-types");
+        t.id = genre.id;
+        t.innerText = genre.name;
+        tagsEl.appendChild(t);
+        t.addEventListener("click", () => {
+            if (selectedGenres.length == 0) {
+                selectedGenres.push(genre.id);
+            } else {
+                if (selectedGenres.includes(genre.id)) {
+                    selectedGenres.forEach((id, idx) => {
+                        if (id == genre.id) {
+                            selectedGenres.splice(idx, 1);
+                        }
+                    });
+                }
+                else {
+                    selectedGenres.push(genre.id);
+                }
+            }
+            // console.log(selectedGenres);
+            getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenres.join(",")));
+            HighlightSelectedGenre();
+            // clearbtn();
+        })
+    });
+
+}
+
+function HighlightSelectedGenre() {
+    const tags = document.querySelectorAll(".genres-types");
+    tags.forEach(tag => {
+        tag.classList.remove("highlight");
+    })
+    clearbtn();
+    if (selectedGenres.length != 0) {
+        selectedGenres.forEach(id => {
+            const highlightedEl = document.getElementById(id);
+            highlightedEl.classList.add("highlight");
+        });
+    }
+}
+
+//Clear button
+function clearbtn() {
+    const clrBtn = document.getElementById("clearBtn");
+    if (clrBtn) {
+        clrBtn.classList.add("highlight");
+    }
+    else {
+        const clear = document.createElement("div");
+        clear.classList.add("genres-types", "highlight");
+        clear.id = "clearBtn";
+        clear.innerText = "Clear x";
+        clear.addEventListener("click", () => {
+            selectedGenres = [];
+            setGenres();
+            getMovies(API_URL);
+        })
+        tagsEl.appendChild(clear);
+    }
+}
+
+//Pagination 
+pre.addEventListener("click", () => {
+    if (prevPage > 0) {
+        pageCall(prevPage);
+    }
+})
+next.addEventListener("click", () => {
+    if (nextPage <= totalPages) {
+        pageCall(nextPage);
+    }
+})
+function pageCall(page) {
+    // ==>> https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d6a90eff85ca8ee94564a85d832ee40a
+    // ==>> https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d6a90eff85ca8ee94564a85d832ee40a&page=2
+    let splitUrl = lastURL.split('?');
+    let queryPara = splitUrl[1].split('&');
+    let pageUrl = queryPara[queryPara.length - 1].split('=');
+    if (pageUrl[0] != 'page') {
+        let url = lastURL + '&page=' + page;
+        getMovies(url);
+    }
+    else {
+        pageUrl[1] = page.toString();
+        let a = pageUrl.join('=');
+        queryPara[queryPara.length - 1] = a;
+        let b = queryPara.join('&');
+        let url = splitUrl[0] + '?' + b;
+        getMovies(url);
+    }
+}
+
+
